84c5e75395164343f45139cf4a57e31939575c86
Nermin Pasalic <nermin.pasalic@abc-softwaredev.com>
2020-03-05 20:27:10 +0100

    Update PetTypeSDJpaServiceIT.java

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -3,8 +3,6 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
